6a114e4ec6e54ff9daae777b0eec0c1200486d3c
juanmagarciat <garciajuanmartin258@gmail.com>
2025-11-01 19:14:57 -0300

    ..

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 #Programa principal
 
-import csv
-
-with open("paieses.csv","r") as archivo:
-    contenido = archivo.read()
-    print(contenido)
-
 import funciones
 
 decision = 1
@@ -14,4 +8,4 @@ while decision != 0:
     funciones.mostrar_menu()
     
     decision = input("Por favor, ingrese una opción: ")
-
+    
