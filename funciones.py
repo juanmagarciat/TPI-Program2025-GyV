b64f78391beda5af8f80cfd427e8cc7d9ce2ad7b
juanmagarciat <garciajuanmartin258@gmail.com>
2025-11-02 18:26:33 -0300

    actualizaciones

--- funciones.py
+++ funciones.py
@@ -74,11 +74,75 @@ def buscar_pais(lista_paises):
 
 #Funcion Filtrar continente.
 
-    def filtrar_por_continente(lista_paises):
-        print("\n2 - Filtrar paises por continente.")
+def filtrar_continente(lista_paises):
+    print("\n2 - Filtrar paises por continente.")
 
-        continentes_disponibles = set()
+    continentes_disponibles = set() #Crea un set con los continentes disponibles, ya que elimina duplicados.
     
-        for pais in lista_paises:
-            continentes_disponibles.add(pais["continente"])
+    for pais in lista_paises: #Recorre la lista de paises, y añande todos los continentes disponibles al set.
+        continentes_disponibles.add(pais["continente"])
     
+    print(f"\nContinentes disponibles en la lista: {', '.join(sorted(continentes_disponibles))}") 
+    
+    continente_buscado = input("\nPor favor, ingrese un continente de la lista: ").strip().capitalize() #Le pide al usuario que ingrese el continente a filtrar.
+
+    if continente_buscado not in continentes_disponibles: #Si el continente buscado no esta en los disponibles indica que no se encuentro.
+        print(f"\nEl continente {continente_buscado} no se encuentra en la lista. ")
+        return
+    
+    paises_continente = [] #Crea la lista de paises del continente buscado, por ahora vacia.
+
+    for pais in lista_paises: #Recorre la lista de paises pais por pais, compara el continente con el buscado y lo agrega a la lista si es necesario.
+        if pais["continente"] == continente_buscado:
+            paises_continente.append(pais)
+    
+    if paises_continente: #Si paises_continente contiene elementos indica cuantos se encontraron del continente buscado.
+        print(f"\nSe han encontrado {len(paises_continente)} en {continente_buscado}...")
+        for pais in paises_continente: #Recorre la lista para mostrar todos los paises y sus datos.
+            _mostrar_pais(pais)
+    else:
+        print(f"\nNo se encontaron paises en {continente_buscado}") #Si la lista esta vacia indica que no se encontraron paises.
+
+#Filtrar por poblacion
+
+def filtrar_poblacion(lista_paises):
+    print("\n2 - Filtrar paises por poblacion.")
+    
+    min_poblacion = _obtener_numero_validado ("Ingrese la poblacion minimna [EJ: 100000] [Vacio para 0]: ",0)
+    max_poblacion = _obtener_numero_validado ("Ingrese la poblacion maxima [EJ: 5000000] [Vacio para sin limite]: ",99999999999)
+
+    if min_poblacion>max_poblacion:
+        print("La poblacion minima no puede ser mayor a la poblacion maxima.")
+        return
+    
+    paises_encontrados = []
+    
+    for pais in lista_paises:
+        if min_poblacion <= pais["poblacion"] <= max_poblacion:
+            paises_encontrados.append(pais)
+    if paises_encontrados:
+        print(f"Se encontraron {len(paises_encontrados)} con poblacion entre {min_poblacion} y {max_poblacion}")
+        for pais in paises_encontrados:
+            _mostrar_pais(pais)
+    else:
+        print("No se encontraron paises en ese rango de problacion.")
+
+
+#Funcion para validar un numero. (Se va utilizar para las funciones de filtrar.)
+
+def _obtener_numero_validado(mensaje, default_valor):
+    while True: #Bucle infinito hasta que obtengam un número
+        entrada_str = input(mensaje).strip()
+        
+        #Opción 1: El usuario presionó Enter
+        if not entrada_str:
+            return default_valor
+        
+        #Opción 2: El usuario ingreso un numero.
+        try:
+            # Intenta convertirlo a entero
+            valor_int = int(entrada_str)
+            return valor_int #Devuelve el numero
+        except ValueError:
+            # Se repite el bucle
+            print(f"Error: '{entrada_str}' no es un número válido. Intente de nuevo.")
